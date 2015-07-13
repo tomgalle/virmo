773a04a75085f3c8d4c260fe1652316f718befd0
tomgalle <admin@TOMGALLECOM.local>
2015-07-13 19:25:30 +0900

    version 2

--- js/script.js
+++ js/script.js
@@ -3,18 +3,21 @@
 (function() {
 
 
-// BASIC VARS
+// VARIABLES
 
 var $windowWidth = $(window).width(),
 $windowHeight = $(window).height(),
 $document = $(document),
-$window = $(window);
+$window = $(window),
+$topSection = $(".top_section"),
+$bottomSection = $(".bottom_section"),
+$section = $(".section"),
+$goDown = $(".godown"),
+$pageOne = $(".page_one");
+
 
 
 // SET PAGES
-
-
-var $section = $(".section");
 
 function setPages() {
 	$windowWidth = $(window).width(),
@@ -28,10 +31,41 @@ setPages();
 
 $window.resize(function(){
 setPages();
+splitHalf();
 });
 
 
+// SPLIT PAGE 7
 
+function splitHalf(){
+	var $windowWidth = $(window).width(),
+	$windowHeight = $(window).height();
+	$topSection.css({height: $windowHeight / 2});
+	$bottomSection.css({
+		height: $windowHeight / 2,
+	// 	position: "absolute",
+	// 	bottom: "0px"
+	});
+
+}
+splitHalf();
+
+
+
+// GO DOWN 1 PAGE
+
+function scrollDown(){
+	$windowWidth = $(window).width(),
+	$windowHeight = $(window).height();
+	$goDown.click(function(){
+		$("html, body").animate({scrollTop: $windowHeight}, 400);
+	});
+
+
+
+
+
+}
 
 
 
