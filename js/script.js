5546fd4ca01aecdb3d42d2c4a3336a1ba780e1a8
Eiji Muroichi <eiji@prty.jp>
2015-07-15 19:53:19 -0400

    Optimized parallax effect

--- js/script.js
+++ js/script.js
@@ -12,7 +12,14 @@ var bottomSection = $(".bottom_section");
 var section = $(".section");
 var goDown = $(".godown");
 var pageOne = $(".page_one");
+var header = $('#header');
 
+var ticking = false;
+var latestScrollY = 0;
+
+var page2PosTop = $(".page_two").position().top;
+var page1Height = $(".page_one").height();
+var page2Height = $(".page_two").height();
 
 // SET PAGES
 
@@ -20,6 +27,10 @@ function setPages() {
 	windowWidth = $(window).width(),
 	windowHeight = $(window).height();
 	// section.css({ "height": windowHeight, "width": windowWidth });
+	page2PosTop = $(".page_two").position().top;
+	page1Height = $(".page_one").height();
+	page2Height = $(".page_two").height();
+
 }
 
 setPages();
@@ -30,54 +41,78 @@ setPages();
 // $(window).resize(function(){
 $(window).on('resize', function(){
 	setPages();
-	splitHalf();
+	// splitHalf();
 });
 
 $(window).on('scroll', function(){
+	onScroll();
+})
 
-	var st = $(window).scrollTop();
+function onScroll(){
+	latestScrollY = window.scrollY;
+	requestTick();
+}
+
+function requestTick(){
+	if(!ticking){
+		requestAnimationFrame(update);
+		ticking = true;
+	}
+}
+
+var jar = $('.jar');
+var pill1 = $('.pill1');
+var pill2 = $('.pill2');
+var pill3 = $('.pill3');
+
+
+function update(){
+
+	ticking = false;
+
+	var st = latestScrollY;
+
+	var dy = page2PosTop - st;
 
 	// header
-	var header = $('#header');
 
-	if(st>$(window).height()){
+	if(st>page1Height){
 		if( !header.hasClass("small-header") ) header.addClass("small-header");
 
 	}else{
 		if( header.hasClass("small-header") ) header.removeClass("small-header");
-
 	}
 
-	// parallax
-	var dy = $(".page_two").position().top - st;
 
-	if(st>0 && st<$('.page_one').height()+$('.page_two').height()){
-		$(".jar").css({
+	// parallax
+	if(st>0 && st<page1Height+page2Height){
+		jar.css({
 			'transform': 'translateY('+ -dy*0.2 +'px) rotate('+(-dy*0.010)+'deg)'
 		})
-		$(".pill1").css({
+		pill1.css({
 			'transform': 'translateY('+ dy*1.2 +'px)'
 		})
 
-		$(".pill2").css({
+		pill2.css({
 			'transform': 'translateY('+ dy*0.4 +'px)'
 		})
 
-		$(".pill3").css({
+		pill3.css({
 			'transform': 'translateY('+ dy*0.1 +'px)'
 		})
 
-		$(".pagetwo_title").css({
+		// $(".pagetwo_title").css({
 			// 'transform': 'translateY('+ -dy*0.4 +'px)'
-		})		
+		// })		
 	}
+}
 
-})
+
 
 
 // SPLIT PAGE 7
 
-function splitHalf(){
+// function splitHalf(){
 	// windowWidth = $(window).width(),
 	// windowHeight = $(window).height();
 	// topSection.css({height: windowHeight / 2});
@@ -87,8 +122,8 @@ function splitHalf(){
 	// 	bottom: "0px"
 	// });
 
-}
-splitHalf();
+// }
+// splitHalf();
 
 
 
